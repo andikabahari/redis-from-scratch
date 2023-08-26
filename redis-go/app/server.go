cadccfbad9431e0b372e899b445eb45234e660a3
Andika Bahari <andikabahari48@gmail.com>
2023-08-26 11:39:34 +0700

    expiry

--- redis-go/app/server.go
+++ redis-go/app/server.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"log"
 	"net"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -25,14 +27,38 @@ func main() {
 	}
 }
 
-var cached map[string]string = make(map[string]string)
+var cached = make(map[string]value)
 
-func set(key, value string) {
-	cached[key] = value
+type value struct {
+	value     string
+	expiredAt time.Time
 }
 
-func get(key string) string {
-	return cached[key]
+func set(k, v string, px int64) {
+	var exp time.Time
+	if px > 0 {
+		now := time.Now().UnixMilli()
+		exp = time.UnixMilli(px + now)
+	}
+	value := value{
+		value:     v,
+		expiredAt: exp,
+	}
+	cached[k] = value
+}
+
+func get(k string) (string, bool) {
+	v, ok := cached[k]
+	if !ok {
+		return "", false
+	}
+	if !v.expiredAt.IsZero() {
+		if time.Now().After(v.expiredAt) {
+			delete(cached, k)
+			return "", false
+		}
+	}
+	return v.value, true
 }
 
 func handleRequest(conn net.Conn) {
@@ -59,14 +85,29 @@ func handleRequest(conn net.Conn) {
 			text := args[4]
 			msg += fmt.Sprintf("+%s\r\n", text)
 		case "set":
+			var px int64
+			if len(args) > 8 {
+				command = args[8]
+				if command == "px" {
+					exp := args[10]
+					px, err = strconv.ParseInt(exp, 10, 64)
+					if err != nil {
+						log.Println("Error parsing int: ", err.Error())
+					}
+				}
+			}
 			key := args[4]
 			value := args[6]
-			set(key, value)
+			set(key, value, px)
 			msg += "+OK\r\n"
 		case "get":
 			key := args[4]
-			value := get(key)
-			msg += fmt.Sprintf("+%s\r\n", value)
+			value, ok := get(key)
+			if ok {
+				msg += fmt.Sprintf("+%s\r\n", value)
+			} else {
+				msg += "*-1\r\n"
+			}
 		default:
 			msg += fmt.Sprintf("-ERR unknown command '%s'\r\n", command)
 		}
